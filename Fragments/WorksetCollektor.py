8ea4b29b302ff51049db602a2f23ea4538ff30c9
Brutich <oleg87arh@gmail.com>
2018-09-27 13:31:09 +0300

    Add "Fragments" folder

---
+++ Fragments/WorksetCollektor.py
@@ -0,0 +1,18 @@
+import clr
+clr.AddReference('RevitAPI')
+from Autodesk.Revit.DB import *
+import Autodesk
+
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+
+doc = UnwrapElement(IN[0])
+worksetTable = doc.GetWorksetTable()
+
+ws_coll = FilteredWorksetCollector(doc);
+ws_kindes = [ws.Kind for ws in ws_coll]
+ws_names = [ws.Name for ws in ws_coll]
+ws_ids = [ws.Id for ws in ws_coll]
+
+OUT = ws_kindes, ws_names, ws_ids
